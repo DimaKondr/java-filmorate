938b4268f477100598389e18f9ba15bd773f1990
Dima Kondratyev <timon.4eb@yandex.ru>
2025-11-19 21:31:04 +0300

    Реализованы модели фильма и пользователя и их классы-контроллеры. Внедрен SpringBoot Реализованы тесты.

---
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTests.java
@@ -0,0 +1,190 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilmControllerTests {
+    private FilmController filmController;
+
+    @BeforeEach
+    void setUp() {
+        filmController = new FilmController();
+    }
+
+    @Test
+    void addValidFilmTesting() {
+        Film film = new Film(null, "Name of the film", "Description of the film",
+                LocalDate.of(1895, Month.DECEMBER, 28), 1L);
+        Film addedFilm = filmController.addFilm(film);
+
+        // Проверяем, что добавился фильм с корректными данными
+        assertNotNull(addedFilm, "Фильм не существует");
+        assertEquals(1, addedFilm.getId(), "ID не совпадает");
+        assertEquals("Name of the film", addedFilm.getName(), "Название не совпадает");
+        assertEquals("Description of the film", addedFilm.getDescription(), "Описание не совпадает");
+        assertEquals(LocalDate.of(1895, 12, 28), addedFilm.getReleaseDate(), "Дата релиза не совпадает");
+        assertEquals(1L, addedFilm.getDuration(), "Длительность не совпадает");
+    }
+
+    @Test
+    void addFilmWithNullRequestTesting() {
+        // Проверяем, что было выброшено необходимое исключение, так как пытаемся добавить null-объект
+        ValidationException exception = assertThrows(ValidationException.class, () -> filmController.addFilm(null),
+                "Исключение не выброшено, или выброшено неверное исключение");
+        assertEquals("Запрос на добавление фильма поступил с пустым телом",
+                exception.getMessage(), "Сообщения не совпадают");
+    }
+
+    @Test
+    void addFilmWithInvalidReleaseDateTesting() {
+        Film film = new Film(null, "Name of the film", "Description of the film",
+                LocalDate.of(1895, Month.DECEMBER, 27), 90L);
+
+        // Проверяем, что было выброшено необходимое исключение, так как некорректная дата релиза
+        ValidationException exception = assertThrows(ValidationException.class, () -> filmController.addFilm(film),
+                "Исключение не выброшено, или выброшено неверное исключение");
+        assertEquals("Дата релиза может быть раньше 28 декабря 1895 года",
+                exception.getMessage(), "Сообщения не совпадают");
+    }
+
+    @Test
+    void updateValidFilmTesting() {
+        Film filmForUpdate = new Film(null, "Name of the film", "Description of the film",
+                LocalDate.of(1895, Month.DECEMBER, 28), 1L);
+        filmController.addFilm(filmForUpdate);
+
+        Film film = new Film(1L, "Updated name of the film", "Updated description of the film",
+                LocalDate.of(1995, Month.DECEMBER, 28), 90L);
+        Film updatedFilm = filmController.updateFilm(film);
+
+        // Проверяем, что пользователь обновился с корректными данными
+        assertNotNull(updatedFilm, "Фильм не существует");
+        assertEquals(1, updatedFilm.getId(), "Изменился ID в процессе обновления");
+        assertEquals("Updated name of the film", updatedFilm.getName(), "Название не обновилось");
+        assertEquals("Updated description of the film", updatedFilm.getDescription(), "Описание не обновилось");
+        assertEquals(LocalDate.of(1995, 12, 28), updatedFilm.getReleaseDate(), "Дата релиза не обновилась");
+        assertEquals(90L, updatedFilm.getDuration(), "Длительность не обновилась");
+    }
+
+    @Test
+    void updateFilmWithNullRequestTesting() {
+        // Проверяем, что было выброшено необходимое исключение, так как пытаемся добавить null-объект
+        ValidationException exception = assertThrows(ValidationException.class,
+                () -> filmController.updateFilm(null),
+                "Исключение не выброшено, или выброшено неверное исключение");
+        assertEquals("Запрос на обновление данных фильма поступил с пустым телом",
+                exception.getMessage(), "Сообщения не совпадают");
+    }
+
+    @Test
+    void updateFilmWithNonExistingIDTesting() {
+        Film film1 = new Film(null, "Name of the film1", "Description of the film1",
+                LocalDate.of(1997, Month.DECEMBER, 15), 60L);
+        Film film2 = new Film(7L, "Name of the film2", "Description of the film2",
+                LocalDate.of(2000, Month.DECEMBER, 29), 70L);
+
+        // Проверяем, что было выброшено необходимое исключение, так как ID имеет значение null
+        ValidationException exception1 = assertThrows(ValidationException.class,
+                () -> filmController.updateFilm(film1),
+                "Исключение не выброшено, или выброшено неверное исключение");
+        assertEquals("ID фильма должен быть указан", exception1.getMessage(), "Сообщения не совпадают");
+
+        // Проверяем, что было выброшено необходимое исключение, так как ID не найден
+        ValidationException exception2 = assertThrows(ValidationException.class,
+                () -> filmController.updateFilm(film2),
+                "Исключение не выброшено, или выброшено неверное исключение");
+        assertEquals("Фильм с ID = " + film2.getId() + " не найден",
+                exception2.getMessage(), "Сообщения не совпадают");
+    }
+
+    @Test
+    void updateFilmWithInvalidReleaseDateTesting() {
+        Film filmForUpdate = new Film(null, "Name of the film", "Description of the film",
+                LocalDate.of(1895, Month.DECEMBER, 28), 1L);
+        filmController.addFilm(filmForUpdate);
+
+        Film film = new Film(1L, "Updated name of the film", "Updated description of the film",
+                LocalDate.of(1895, Month.DECEMBER, 27), 90L);
+        Film updatedFilm = filmController.updateFilm(film);
+
+        // Проверяем, что фильм не обновил данные на некорректные
+        assertNotNull(updatedFilm, "Фильм не существует");
+        assertEquals(LocalDate.of(1895, 12, 28), updatedFilm.getReleaseDate(), "Дата релиза некорректна");
+    }
+
+    @Test
+    void getAllFilmsTesting() {
+        Film film1 = new Film(null, "Name of the film1", "Description of the film1",
+                LocalDate.of(1995, Month.DECEMBER, 29), 90L);
+        Film film2 = new Film(null, "Name of the film2", "Description of the film2",
+                LocalDate.of(1900, Month.DECEMBER, 29), 120L);
+        Film film3 = new Film(null, "Name of the film3", "Description of the film3",
+                LocalDate.of(2001, Month.DECEMBER, 29), 145L);
+        filmController.addFilm(film1);
+        filmController.addFilm(film2);
+        filmController.addFilm(film3);
+        List<Film> allFilms = filmController.getAllFilms();
+
+        // Проверяем, что список существует, а также количество пользователей
+        assertNotNull(allFilms);
+        assertEquals(3, allFilms.size());
+
+        // Создаем тестовых пользователей
+        Film testFilm1 = new Film(1L, "Name of the film1", "Description of the film1",
+                LocalDate.of(1995, Month.DECEMBER, 29), 90L);
+        Film testFilm2 = new Film(2L, "Name of the film2", "Description of the film2",
+                LocalDate.of(1900, Month.DECEMBER, 29), 120L);
+        Film testFilm3 = new Film(3L, "Name of the film3", "Description of the film3",
+                LocalDate.of(2001, Month.DECEMBER, 29), 145L);
+
+        List<Film> testList = new ArrayList<>();
+        testList.add(testFilm1);
+        testList.add(testFilm2);
+        testList.add(testFilm3);
+
+        // Проверяем, что списки совпадают
+        assertEquals(testList, allFilms, "Списки не совпадают");
+    }
+
+    @Test
+    void getNextIdTesting() {
+        Film film1 = new Film(null, "Name of the film1", "Description of the film1",
+                LocalDate.of(1995, Month.DECEMBER, 29), 90L);
+        Film film2 = new Film(null, "Name of the film2", "Description of the film2",
+                LocalDate.of(1900, Month.DECEMBER, 29), 120L);
+        Film film3 = new Film(null, "Name of the film3", "Description of the film3",
+                LocalDate.of(2001, Month.DECEMBER, 29), 145L);
+        Film addedFilm1 = filmController.addFilm(film1);
+        Film addedFilm2 = filmController.addFilm(film2);
+        Film addedFilm3 = filmController.addFilm(film3);
+
+        // Проверяем, что назначен ожидаемый ID.
+        assertEquals(1, addedFilm1.getId(), "Генерация ID не работает");
+        assertEquals(2, addedFilm2.getId(), "Генерация ID не работает");
+        assertEquals(3, addedFilm3.getId(), "Генерация ID не работает");
+
+        Film testFilm1 = new Film(1L, "Name of the film1", "Description of the film1",
+                LocalDate.of(1995, Month.DECEMBER, 29), 90L);
+        Film testFilm2 = new Film(3L, "Name of the film2", "Description of the film2",
+                LocalDate.of(1900, Month.DECEMBER, 29), 120L);
+        Film testFilm3 = new Film(10L, "Name of the film3", "Description of the film3",
+                LocalDate.of(2001, Month.DECEMBER, 29), 145L);
+        Film addedTestFilm1 = filmController.addFilm(testFilm1);
+        Film addedTestFilm2 = filmController.addFilm(testFilm2);
+        Film addedTestFilm3 = filmController.addFilm(testFilm3);
+
+        // Проверим, что был переназначен верный ID.
+        assertEquals(4, addedTestFilm1.getId(), "Генерация ID не работает");
+        assertEquals(5, addedTestFilm2.getId(), "Генерация ID не работает");
+        assertEquals(6, addedTestFilm3.getId(), "Генерация ID не работает");
+    }
+}
---
+++ src/test/java/ru/yandex/practicum/filmorate/model/FilmTests.java
@@ -0,0 +1,148 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilmTests {
+    private static ValidatorFactory validatorFactory;
+    private static Validator validator;
+
+    @BeforeAll
+    static void setUp() {
+        validatorFactory = Validation.buildDefaultValidatorFactory();
+        validator = validatorFactory.getValidator();
+    }
+
+    @AfterAll
+    static void tearDown() {
+        if (validatorFactory != null) {
+            validatorFactory.close();
+        }
+    }
+
+    @Test
+    void testValidFilm() {
+        StringBuilder sb = new StringBuilder();
+        String description200 = sb.repeat("Y", 200).toString();
+
+        assertEquals(200, description200.length(), "Неверная длина описания");
+
+        Film film1 = new Film(1L, "Тестовый фильм", "Очень интересное кино",
+                LocalDate.of(2000, Month.JANUARY, 15), 90L);
+        Film film2 = new Film(1L, "Тестовый фильм", "",
+                LocalDate.of(2000, Month.JANUARY, 15), 90L);
+        Film film3 = new Film(1L, "Тестовый фильм", description200,
+                LocalDate.of(2000, Month.JANUARY, 15), 90L);
+        Set<ConstraintViolation<Film>> violations1 = validator.validate(film1);
+        Set<ConstraintViolation<Film>> violations2 = validator.validate(film2);
+        Set<ConstraintViolation<Film>> violations3 = validator.validate(film3);
+
+        assertTrue(violations1.isEmpty());
+        assertTrue(violations2.isEmpty());
+        assertTrue(violations3.isEmpty());
+    }
+
+    @Test
+    void testInvalidName() {
+        Film film1 = new Film(1L, null, "Очень интересное кино",
+                LocalDate.of(2000, Month.JANUARY, 15), 90L);
+        Film film2 = new Film(1L, "", "Очень интересное кино",
+                LocalDate.of(2000, Month.JANUARY, 15), 90L);
+        Set<ConstraintViolation<Film>> violations1 = validator.validate(film1);
+        Set<ConstraintViolation<Film>> violations2 = validator.validate(film2);
+
+        assertFalse(violations1.isEmpty(), "Должны быть нарушения");
+        assertFalse(violations2.isEmpty(), "Должны быть нарушения");
+
+        List<ConstraintViolation<Film>> violationsList1= violations1.stream().toList();
+        List<ConstraintViolation<Film>> violationsList2= violations2.stream().toList();
+        String message1 = violationsList1.get(0).getMessage();
+        String message2 = violationsList2.get(0).getMessage();
+
+        assertEquals("Название не может быть null или пустым", message1, "Неверное сообщение об ошибке");
+        assertEquals("Название не может быть null или пустым", message2, "Неверное сообщение об ошибке");
+    }
+
+    @Test
+    void testInvalidDescription() {
+        StringBuilder sb = new StringBuilder();
+        String description201 = sb.repeat("Y", 201).toString();
+
+        assertTrue(description201.length() > 200, "Короткое описание");
+
+        Film film1 = new Film(1L, "Тестовый фильм", null,
+                LocalDate.of(2000, Month.JANUARY, 15), 90L);
+        Film film2 = new Film(1L, "Тестовый фильм", description201,
+                LocalDate.of(2000, Month.JANUARY, 15), 90L);
+        Set<ConstraintViolation<Film>> violations1 = validator.validate(film1);
+        Set<ConstraintViolation<Film>> violations2 = validator.validate(film2);
+
+        assertFalse(violations1.isEmpty(), "Должны быть нарушения");
+        assertFalse(violations2.isEmpty(), "Должны быть нарушения");
+
+        List<ConstraintViolation<Film>> violationsList1= violations1.stream().toList();
+        List<ConstraintViolation<Film>> violationsList2= violations2.stream().toList();
+        String message1 = violationsList1.get(0).getMessage();
+        String message2 = violationsList2.get(0).getMessage();
+
+        assertEquals("Описание не может быть null", message1, "Неверное сообщение об ошибке");
+        assertEquals("Максимальная длина описания — 200 символов", message2, "Неверное сообщение об ошибке");
+    }
+
+    @Test
+    void testInvalidReleaseDate() {
+        Film film = new Film(1L, "Тестовый фильм", "Очень интересное кино",
+                null, 90L);
+        Set<ConstraintViolation<Film>> violations = validator.validate(film);
+
+        assertFalse(violations.isEmpty(), "Должны быть нарушения");
+
+        List<ConstraintViolation<Film>> violationsList= violations.stream().toList();
+        String message = violationsList.get(0).getMessage();
+
+        assertEquals("Дата релиза не может быть null", message, "Неверное сообщение об ошибке");
+    }
+
+    @Test
+    void testInvalidDuration() {
+        Film film1 = new Film(1L, "Тестовый фильм", "Очень интересное кино",
+                LocalDate.of(2000, Month.JANUARY, 15), null);
+        Film film2 = new Film(1L, "Тестовый фильм", "Очень интересное кино",
+                LocalDate.of(2000, Month.JANUARY, 15), 0L);
+        Film film3 = new Film(1L, "Тестовый фильм", "Очень интересное кино",
+                LocalDate.of(2000, Month.JANUARY, 15), -1L);
+
+        Set<ConstraintViolation<Film>> violations1 = validator.validate(film1);
+        Set<ConstraintViolation<Film>> violations2 = validator.validate(film2);
+        Set<ConstraintViolation<Film>> violations3 = validator.validate(film3);
+
+        assertFalse(violations1.isEmpty(), "Должны быть нарушения");
+        assertFalse(violations2.isEmpty(), "Должны быть нарушения");
+        assertFalse(violations3.isEmpty(), "Должны быть нарушения");
+
+        List<ConstraintViolation<Film>> violationsList1= violations1.stream().toList();
+        List<ConstraintViolation<Film>> violationsList2= violations2.stream().toList();
+        List<ConstraintViolation<Film>> violationsList3= violations3.stream().toList();
+        String message1 = violationsList1.get(0).getMessage();
+        String message2 = violationsList2.get(0).getMessage();
+        String message3 = violationsList3.get(0).getMessage();
+
+        assertEquals("Продолжительность не может быть null", message1, "Неверное сообщение об ошибке");
+        assertEquals("Продолжительность фильма должна быть положительным числом",
+                message2, "Неверное сообщение об ошибке");
+        assertEquals("Продолжительность фильма должна быть положительным числом",
+                message3, "Неверное сообщение об ошибке");
+    }
+}
