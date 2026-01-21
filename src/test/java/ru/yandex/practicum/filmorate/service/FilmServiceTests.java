ee0d113c662a76f22b2ebfadd584234de6177508
Olga Zavalishina <steffane@yandex.ru>
2026-01-21 17:35:30 +0300

    checkstyle

--- src/test/java/ru/yandex/practicum/filmorate/service/FilmServiceTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/service/FilmServiceTests.java
@@ -30,7 +30,6 @@ import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
